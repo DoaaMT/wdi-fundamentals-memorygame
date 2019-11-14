84e241bfd79946838d0df399c04f9a1cd98231e2
DoaaMT <doaa.turkistani@hotmail.com>
2019-11-14 12:32:00 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,1 +1,34 @@
 console.log("Up and running!");
+
+
+console.log("User flipped " + CardOne);
+
+alert('Hello, friends.');
+
+const Cards =['queen','queen','king','king'] ;
+const CardsInPlay= [] ;
+
+var CardOne = Cards[0];
+CardsInPlay.push(CardOne);
+
+console.log("User flipped queen")
+
+var CardTwo = Cards[2];
+CardsInPlay.push(CardTwo);
+
+console.log("User flipped king")
+
+if (CardsInPlay.length == 2) {
+if (CardsInPlay[0]===CardsInPlay[1] ) {
+  alert("You found a match!");
+}
+else{
+  alert("Sorry, try again.");
+}
+}
+
+
+
+
+//var cardThree= "king";
+//var cardFour = "king";
